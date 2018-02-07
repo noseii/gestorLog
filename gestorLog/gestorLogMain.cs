77a09dde234e5395c50b407c7f47ca3efe9b6ab6
NoseII <noseii@gmail.com>
2018-02-07 11:58:27 +0100

    Prueba actualizacion

--- gestorLog/gestorLogMain.cs
+++ gestorLog/gestorLogMain.cs
@@ -20,6 +20,8 @@ namespace gestorLog
 
         internal string rutaFichero = string.Empty;
         internal Basicos.Modos modo = Basicos.Modos.Completo;
+        internal int idEvento = 666;
+        internal Int16 categoria = 0x00;
 
         /// =============================================================================================== ///
         /// Para que funcione la publicación en el visor de eventos, hay que crear la siguiente             ///
@@ -28,22 +30,36 @@ namespace gestorLog
         /// Donde Prueba es el nombre de origen en el visor de eventos                                      ///
         /// =============================================================================================== ///
         // Mismo nombre que la entrada que hemos creado en el registro
-        internal const string APLICACION = "Prueba";
+        internal const string APLICACION = "Application";
+        internal string aplicacion = string.Empty;
         #endregion Fin --> Variables globales
 
         #region Constructores
+        /// <summary>
+        /// Constructor, para el manejo de sucesos  
+        /// </summary>
+        /// <param name="_modo"></param>
         public gestorLogMain(Basicos.Modos _modo)
         {
             modo = _modo;
         }
-
+        /// <summary>
+        /// Constructor, para el manejo de fichero 
+        /// </summary>
+        /// <param name="_fichero"></param>
+        /// <param name="_rutaFichero"></param>
         public gestorLogMain(string _fichero, string _rutaFichero)
         {
             nombreFichero = _fichero;
             rutaFichero = _rutaFichero;
             modo = Basicos.Modos.Fichero;
         }
-
+        /// <summary>
+        /// Constructor general
+        /// </summary>
+        /// <param name="_fichero"></param>
+        /// <param name="_rutaFichero"></param>
+        /// <param name="_modo"></param>
         public gestorLogMain(string _fichero, string _rutaFichero, Basicos.Modos _modo)
         {
             nombreFichero = _fichero;
@@ -53,26 +69,61 @@ namespace gestorLog
         #endregion Fin --> Constructores
 
         #region Propiedades
+        /// <summary>
+        /// NOmbre del fichero de Log
+        /// </summary>
         public string NombreFichero
         {
             get { return nombreFichero; }
             set { nombreFichero = value; }
         }
-
+        /// <summary>
+        /// Ruta donde se encuentra el fichero de Log
+        /// </summary>
         public string RutaFichero
         {
             get { return rutaFichero; }
             set { rutaFichero = value; }
         }
-
+        /// <summary>
+        /// Modos de importancia del mensaje
+        /// </summary>
         public Basicos.Modos Modo
         {
             get { return modo; }
             set { modo = value; }
         }
+        /// <summary>
+        /// Id del evento, solo visor de eventos
+        /// </summary>
+        public int IdEvento
+        {
+            get { return idEvento; }
+            set { idEvento = value; }
+        }
+        /// <summary>
+        /// Categoria del evento, solo visor de eventos
+        /// </summary>
+        public Int16 CategoriaEvento
+        {
+            get { return categoria; }
+            set { categoria = value; }
+        }
+
+        public string Aplicacion
+        {
+            get { return aplicacion; }
+            set { aplicacion = value; }
+        }
         #endregion Fin --> Propiedades
 
-
+        #region Formateo del mensaje
+        /// <summary>
+        /// Formateo del texto del mensale
+        /// </summary>
+        /// <param name="_mensaje">Mensaje a publicar</param>
+        /// <param name="_nivel">Nivel de advertencias</param>
+        /// <returns></returns>
         internal string formatoMensaje(string _mensaje, Basicos.Niveles _nivel)
         {
             // Variables gestion respuesta
@@ -113,7 +164,7 @@ namespace gestorLog
 
             // Formatear fecha y hora
             DateTime dtFechaHora = DateTime.Now;
-            strFechaHora = string.Format("{0:MM/dd/yyyy - HH:mm:ss}", dtFechaHora);
+            strFechaHora = string.Format("{0:dd/MM/yyyy - HH:mm:ss}", dtFechaHora);
 
             // Generar mensaje
             strRespuesta = string.Format("{0} {1} --> {2}", strNivel, strFechaHora, mensaje);
@@ -121,32 +172,59 @@ namespace gestorLog
             // Respuesta
             return strRespuesta;
         }
+        #endregion Fin --> Formateo mensaje
 
         #region Gestor de sucesos de Windows
-        public bool Sucessos(string _mensaje, Basicos.Niveles _nivel, out KeyValuePair<string, string> _strError)
-        {
-            bool bRespuesta = false;
-
-            bRespuesta = grabarSucesos(_mensaje, _nivel, out _strError);
-
-            // Respuesta
-            return bRespuesta;
-        }
-
-        internal bool grabarSucesos(string _mensaje, Basicos.Niveles _nivel, out KeyValuePair<string, string> _strError)
+        /// <summary>
+        /// Para verificar que la publicación en el visor de sucesos funciona
+        /// Quitar al final
+        /// </summary>
+        /// <param name="_mensaje"></param>
+        /// <param name="_nivel"></param>
+        /// <param name="_strError"></param>
+        /// <returns></returns>
+        // public bool Sucessos(string _mensaje, Basicos.Niveles _nivel, out KeyValuePair<string, string> _strError)
+        // {
+        //     bool bRespuesta = false;
+        // 
+        //     bRespuesta = grabarSucesos(_mensaje, _nivel, out _strError);
+        // 
+        //     // Respuesta
+        //     return bRespuesta;
+        // }
+        /// <summary>
+        /// Función para publicar en el gestor de eventos
+        /// </summary>
+        /// <param name="_mensaje">Mensaje que vamos a publicar</param>
+        /// <param name="_nivel">Nivel de importancia del mensaje</param>
+        /// <param name="_strError">Si se produce un error, devuelve la descripción de el</param>
+        /// <returns>True --> ejecución correcta, False --> Se ha producido un error en la ejecución</returns>
+        /// <remarks>Para que funcione bien tiene que estar creada la clave en el registro.
+        /// Para que funcione la publicación en el visor de eventos, hay que crear la siguiente clave en el registro:
+        /// HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Application\Prueba
+        /// Donde "Prueba" es el nombre de origen en el visor de eventos</remarks>
+        internal bool publicarSucesos(string _mensaje, Basicos.Niveles _nivel, out KeyValuePair<string, string> _strError)
         {
             // Variables gestion respuesta
             bool bRespuesta = false;
             KeyValuePair<string, string> strError = new KeyValuePair<string, string>();
 
-
             // Variables internas
             string sSource;
             string mensaje = _mensaje;
+            string mensajeGrabar = string.Empty;
             Basicos.Niveles nivel = _nivel;
             EventLogEntryType nivelEvento = new EventLogEntryType();
             // Establece el origen en el visor de eventos
-            sSource = APLICACION;
+            if (string.IsNullOrEmpty(aplicacion))
+            {
+                sSource = APLICACION;
+            }
+            else
+            {
+                sSource = aplicacion;
+            }
+            
 
             // Extraer nivel
             switch(nivel)
@@ -177,12 +255,12 @@ namespace gestorLog
                     break;
             }
 
-
+            // Formatear mensaje
+            mensajeGrabar = formatoMensaje(mensaje, nivel);
 
             try
             {
-                EventLog.WriteEntry(sSource, mensaje, nivelEvento, 01, 01);
-
+                EventLog.WriteEntry(sSource, mensajeGrabar, nivelEvento, idEvento, categoria);
                 bRespuesta = true;
             }
             catch (Exception ex)
@@ -197,20 +275,33 @@ namespace gestorLog
         }
         #endregion Fin - Gestor sucesos
 
-
         #region Grabar en fichero
-        public bool Ficheros(string _mensaje, Basicos.Niveles _nivel, out KeyValuePair<string, string> _strError)
-        {
-            // Variables gestion respuesta
-            bool bRespuesta = false;
-
-            bRespuesta = grabarFichero(_mensaje, _nivel, out _strError);
-
-            // Respuesta
-            return bRespuesta;
-        }
-        
-        internal bool grabarFichero(string _mensaje, Basicos.Niveles _nivel, out KeyValuePair<string, string> _strError)
+        /// <summary>
+        /// Función para la pruebas unitarias
+        /// Quitar al final
+        /// </summary>
+        /// <param name="_mensaje"></param>
+        /// <param name="_nivel"></param>
+        /// <param name="_strError"></param>
+        /// <returns></returns>
+        // public bool Ficheros(string _mensaje, Basicos.Niveles _nivel, out KeyValuePair<string, string> _strError)
+        // {
+        //     // Variables gestion respuesta
+        //     bool bRespuesta = false;
+        // 
+        //     bRespuesta = grabarFichero(_mensaje, _nivel, out _strError);
+        // 
+        //     // Respuesta
+        //     return bRespuesta;
+        // }
+        /// <summary>
+        /// Función que escribe en un fichero de log, los mensajes
+        /// </summary>
+        /// <param name="_mensaje">Mensaje que deseamos publicar</param>
+        /// <param name="_nivel">Nivel de imporancia del mensaje</param>
+        /// <param name="_strError">Si se produce un error, devuelve la descripción de el</param>
+        /// <returns>True --> ejecución correcta, False --> Se ha producido un error en la ejecución</returns>
+        internal bool publicarFichero(string _mensaje, Basicos.Niveles _nivel, out KeyValuePair<string, string> _strError)
         {
             // Variables para gestionar la respuesta
             bool bRespuesta = false;
@@ -258,5 +349,51 @@ namespace gestorLog
             return bRespuesta;
         }
         #endregion Fin --> Grabar en fichero
+
+        #region Publicar mensaje Log y Evento
+        public bool publicarLog(string _mensaje, Basicos.Niveles _nivel, out KeyValuePair<string, string> _strError)
+        {
+            // Variables para procesar la respuesta
+            bool bRespuesta = false;
+            KeyValuePair<string, string> strError = new KeyValuePair<string, string>();
+
+            // Variables internas
+            string mensaje = _mensaje;
+            Basicos.Niveles nivel = _nivel;
+
+            if ((modo != Basicos.Modos.Completo) && (modo != Basicos.Modos.Fichero) && (modo != Basicos.Modos.Sistema))
+            {
+                strError = new KeyValuePair<string, string>("ERROR_MODO_NO_VALIDO", "Error en el modo de publicación.");
+                bRespuesta = false;
+            }
+            else
+            {
+                if ((modo == Basicos.Modos.Completo) || (modo == Basicos.Modos.Fichero))
+                {
+                    // Publicamos en el fichero
+                    bRespuesta = publicarFichero(mensaje, nivel, out strError);
+                }
+                else
+                {
+                    // Nada
+                }
+
+                if ((modo == Basicos.Modos.Completo) || (modo == Basicos.Modos.Sistema))
+                {
+                    // Publicamos en el visor de sucesos
+                    bRespuesta = publicarSucesos(mensaje, nivel, out strError);
+                }
+                else
+                {
+                    // Nada
+                }
+            }
+
+
+            // Respuesta
+            _strError = strError;
+            return bRespuesta;
+        }
+        #endregion Fin --> Publicar
     }
 }
--- gestorLog_Tests/gestorLogMain_Tests.cs
+++ gestorLog_Tests/gestorLogMain_Tests.cs
@@ -14,6 +14,7 @@ namespace gestorLog.Tests
         [Test()]
         public void grabarFichero_Test()
         {
+            bool bRespuesta = false;
             KeyValuePair<string, string> strError = new KeyValuePair<string, string>();
 
             string path = (System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase.ToString()).Remove(0, 6)) + "\\prueba_text.log";
@@ -24,7 +25,7 @@ namespace gestorLog.Tests
             Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().Location);
             Console.WriteLine(mydocpath);
 
-            bool bRespuesta = gestorLog.Ficheros("Esto es una prueba", Basicos.Niveles.Ninguno, out strError);
+            // bool bRespuesta = gestorLog.Ficheros("Esto es una prueba", Basicos.Niveles.Ninguno, out strError);
 
             Console.WriteLine(strError.Value);
 
@@ -35,8 +36,46 @@ namespace gestorLog.Tests
         public void Sucessos_Test()
         {
             bool bRespuesta = false;
+            KeyValuePair<string, string> strError = new KeyValuePair<string, string>();
 
             gestorLogMain gestorLog = new gestorLogMain(Basicos.Modos.Sistema);
+
+            // bRespuesta = gestorLog.Sucessos("Esto es un mensaje de prueba", Basicos.Niveles.Error, out strError);
+
+            Assert.True(bRespuesta);
+        }
+
+        [Test()]
+        public void publicarLog_Test()
+        {
+            bool bRespuesta = false;
+            KeyValuePair<string, string> strError = new KeyValuePair<string, string>();
+
+            string path = (System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase.ToString()).Remove(0, 6)) + "\\prueba_text.log";
+            string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            gestorLogMain gestorLog = new gestorLogMain("\\prueba_text_1.log", mydocpath);
+
+            bRespuesta = gestorLog.publicarLog("Esto es una prueba", Basicos.Niveles.Error, out strError);
+
+            Console.WriteLine(strError.Value);
+
+            Assert.True(bRespuesta);
+        }
+
+        [Test()]
+        public void publicarLog_Test1()
+        {
+            bool bRespuesta = false;
+            KeyValuePair<string, string> strError = new KeyValuePair<string, string>();
+
+            gestorLogMain gestorLog = new gestorLogMain(Basicos.Modos.Sistema);
+            gestorLog.Aplicacion = "Prueba";
+            gestorLog.IdEvento = 999;
+            gestorLog.CategoriaEvento = 1;
+
+            bRespuesta = gestorLog.publicarLog("Esto es un mensaje de prueba", Basicos.Niveles.Error, out strError);
+
+            Console.WriteLine(strError.Value);
 
             Assert.True(bRespuesta);
         }
